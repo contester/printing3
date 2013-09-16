ade6ad500a6a6e02e872b5d9b85d858af01c91e8
Paul Komkoff <i@stingr.net>
2013-09-16 22:53:25 +0100

    ...

--- tools/config.go
+++ tools/config.go
@@ -1,22 +1,91 @@
 package tools
 
-import "github.com/jjeffery/stomp"
+import (
+	"github.com/jjeffery/stomp"
+	"regexp"
+	"strings"
+	"code.google.com/p/goconf/conf"
+)
 
-type MessagingConfig struct {
-	Address string
-	Login string
-	Passcode string
-	Host string
+type StompConfig struct {
+	Network, Address string
+	Options stomp.Options
 }
 
-func (s *MessagingConfig) NewConnection() (*stomp.Conn, error) {
-	var opts stomp.Options
-	opts.AcceptVersion = "1.1,1.2"
-	if s.Login != "" {
-		opts.Login, opts.Passcode = s.Login, s.Passcode
+func (s *StompConfig) NewConnection() (*stomp.Conn, error) {
+	return stomp.Dial(s.Network, s.Address, s.Options)
+}
+
+var dsnPattern = regexp.MustCompile(
+			`^(?:(?P<user>.*?)(?::(?P<passwd>.*))?@)?` + // [user[:password]@]
+					`(?:(?P<net>[^\(]*)(?:\((?P<addr>[^\)]*)\))?)?` + // [net[(addr)]]
+				`\/(?P<vhost>.*?)` + // /dbname
+			`(?:\?(?P<params>[^\?]*))?$`) // [?param1=value1&paramN=valueN]
+
+func ParseStompDSN(dsn string) (config *StompConfig, err error) {
+	if dsn == "" {
+		return
 	}
-	if s.Host != "" {
-		opts.Host = s.Host
+
+	config = &StompConfig{}
+
+	matches := dsnPattern.FindStringSubmatch(dsn)
+	names := dsnPattern.SubexpNames()
+
+	for i, match := range matches {
+		switch names[i] {
+		case "user":
+			config.Options.Login = match
+		case "passwd":
+			config.Options.Passcode = match
+		case "net":
+			config.Network = match
+		case "addr":
+			config.Address = match
+		case "vhost":
+			config.Options.Host = match
+		case "params":
+			for _, v := range strings.Split(match, "&") {
+				param := strings.SplitN(v, "=", 2)
+				if len(param) != 2 {
+					continue
+				}
+				config.Options.NonStandard.Add(param[0], param[1])
+			}
+		}
 	}
-	return stomp.Dial("tcp", s.Address, opts)
+
+	// Set default network if empty
+	if config.Network == "" {
+		config.Network = "tcp"
+	}
+
+	// Set default adress if empty
+	if config.Address == "" {
+		config.Address = "127.0.0.1:61613"
+	}
+
+	return
+}
+
+func ParseStompConfig(config *conf.ConfigFile, section string) (result *StompConfig, err error) {
+	var cf StompConfig
+	cf.Network = "tcp"
+	if cf.Address, err = config.GetString("messaging", "address"); err != nil {
+		return
+	}
+
+	if cf.Options.Login, err = config.GetString("messaging", "username"); err != nil {
+		return
+	}
+
+	if cf.Options.Passcode, err = config.GetString("messaging", "password"); err != nil {
+		return
+	}
+
+	if cf.Options.Host, err = config.GetString("messaging", "vhost"); err != nil {
+		return
+	}
+
+	return &cf, nil
 }
