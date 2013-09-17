736b1e8996824fcb6c21c8b821e900aa55f83e41
Paul Komkoff <i@stingr.net>
2013-09-17 08:23:15 +0100

    ...

--- ticket_grabber/ticket_grabber.go
+++ ticket_grabber/ticket_grabber.go
@@ -24,6 +24,29 @@ func createDb(spec string) (db *sql.DB, err error) {
 	return
 }
 
+const SELECT_QUERY = `select
+a.Contest, a.Team, a.Touched, a.Task, a.Compiled,
+(unix_timestamp(a.Arrived) - unix_timestamp(Contests.Start)) as Arrived,
+a.Passed, a.Taken, a.ID, count(b.ID) as SubmitNo, Areas.Printer,
+Contests.SchoolMode, a.TestingID, Schools.Name, Teams.Num,
+Contests.Name as ContestName, Problems.Name as ProblemName,
+inet_ntoa(a.Computer) as ComputerID, CompLocations.Name as ComputerName,
+Areas.ID as AreaID, Areas.Name as AreaName
+from
+Submits as a, Submits as b, Contests, Areas, CompLocations, Teams, Schools, Participants, Problems
+Contests.ID = a.Contest, a.Finished, a.Contest = b.Contest, a.Team = b.Team,
+a.Task = b.Task, b.Arrived <= a.Arrived, ((a.Printed is null) or (a.Printed < a.Touched)),
+a.Computer = CompLocations.ID, CompLocations.Location = Areas.ID, Participants.LocalID = a.Team,
+Teams.ID = Participants.Team, Participants.Contest = a.Contest, Teams.School = Schools.ID,
+Problems.Contest = a.Contest, Problems.ID = a.Task, Contests.PrintTickets = 1, Areas.Printer is not NULL
+group by a.Task, a.Compiled, a.Arrived, a.Passed, a.Taken, a.ID
+order by a.Touched asc
+`
+
+func scan(db *sql.DB) {
+	db.QueryRow()
+}
+
 func main() {
 	tools.SetupLogWrapper()
 	defer log4go.Close()
