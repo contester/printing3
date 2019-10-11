379c2aa88d7ac91a75b5274b3c3b26367f5c7d84
Paul P Komkoff Jr <stingray@main.sgu.ru>
2019-10-11 12:05:21 +0400

    huh

--- tools/receive.go
+++ tools/receive.go
@@ -48,17 +48,19 @@ func SubscribeAndProcess(ctx context.Context, conn *stomp.Conn, queue string, pr
 	}
 
 	go func() {
-		select {
-		case v, ok := <-sub.C:
-			if !ok {
-				log.Fatalf("subscription %q closed", sub)
+		for {
+			select {
+			case v, ok := <-sub.C:
+				if !ok {
+					log.Fatalf("subscription %q closed", sub)
+				}
+				if err := proc(ctx, v); err != nil {
+					log.Fatalf("proc error: %v", err)
+				}
+			case <-ctx.Done():
+				sub.Unsubscribe()
+				return
 			}
-			if err := proc(ctx, v); err != nil {
-				log.Fatalf("proc error: %v", err)
-			}
-		case <-ctx.Done():
-			sub.Unsubscribe()
-			return
 		}
 	}()
 	return sub, nil
