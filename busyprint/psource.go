7e20316ef561a83a97d9956fd588129500aa69ce
Paul Komkoff <i@stingr.net>
2019-10-16 22:31:47 +0400

    ...

--- busyprint/psource.go
+++ busyprint/psource.go
@@ -4,18 +4,18 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"html/template"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"text/template"
 
 	tpb "github.com/contester/printing3/tickets"
 )
 
 const documentTemplateString = `\documentclass[12pt,a4paper,oneside]{article}
-\usepackage[cp1251]{inputenc}
+\usepackage[utf8]{inputenc}
 \usepackage[english,russian]{babel}
 \usepackage{fancyhdr}
 \usepackage{fancyvrb}
@@ -69,6 +69,7 @@ var documentTemplate = template.Must(template.New("source").Parse(documentTempla
 type templateData struct {
 	*tpb.PrintJob
 	StyleText, IncludeText string
+	Encoding               string
 }
 
 func texEscape(s string) string {
@@ -109,7 +110,7 @@ func (s *server) processSource(ctx context.Context, job *tpb.PrintJob) ([]byte, 
 		sourceCharset = "cp1251"
 	}
 
-	args := []string{"-l", "text", "-f", "latex", "-O", "linenos=1,tabsize=4,encoding=" + sourceCharset, "-o", outputName, sourceName}
+	args := []string{"-l", "text", "-f", "latex", "-O", "linenos=1,tabsize=4,outencoding=utf8,encoding=" + sourceCharset, "-o", outputName, sourceName}
 	cmd := exec.Command("pygmentize", args...)
 	cmd.Dir, cmd.Stdin, cmd.Stdout, cmd.Stderr = jobDir, os.Stdin, os.Stdout, os.Stderr
 	if err := cmd.Run(); err != nil {
--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/golang/protobuf v1.3.2
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/sirupsen/logrus v1.4.2
+	github.com/wercker/journalhook v0.0.0-20180428041537-5d0a5ae867b3 // indirect
 	golang.org/x/net v0.0.0-20191007182048-72f939374954 // indirect
 )
